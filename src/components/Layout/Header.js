78f2ec402355ef8d240802f6690b12ccac6f3d21
Agata <agata.maryanska@gmail.com>
2022-01-09 18:27:20 +0100

    add modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 
-const Header = props => {
+const Header = (props) => {
     return (
     <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table of food" />
